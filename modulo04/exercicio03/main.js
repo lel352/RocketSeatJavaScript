09593d7d11cb33a225e533577e58ea4cca819e33
Leandro <leandro@pcsistemas.com.br>
2020-07-15 11:35:41 -0300

    Modulo 4 - Exercicio 1 a 3

--- modulo04/exercicio03/main.js
+++ modulo04/exercicio03/main.js
@@ -23,6 +23,7 @@ function limpar() {
 }
 
 function mostrarRepositorio(repositorios) {
+    limpar();
     for (repositorio of repositorios) {
         criandoLinha(repositorio.name);
     }
@@ -38,11 +39,18 @@ function criandoLinha(texto) {
 
 function buscarDados(nome) {
     limpar();
+    criandoLinha('Carregando...');
     axios.get('https://api.github.com/users/' + nome + '/repos')
         .then(function (response) {
             if (response.data.length > 0)
                 mostrarRepositorio(response.data);
-            else
+            else {
+                limpar();
                 repostaElement.innerHTML = 'Dados não foram encontrados. ';
+            }
         })
+        .catch(function (error) {
+            limpar();
+            repostaElement.innerHTML = 'Erro ao buscar dados. ' + error;
+        });
 }
